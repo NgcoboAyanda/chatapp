bf68095cf996cf24679bcdc3638d65d89eb3bac9
Ayanda Ngcobo <ayanda13@protonmail.com>
2023-04-27 11:04:25 +0200

    initialized project, added header components

---
+++ src/components/MessagesHeader/MessagesHeader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './MessagesHeader.css';
+
+const MessageHeader = () => {
+    return (
+        <div className="messages-header">
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageHeader;
