f9e0be8b875d4aed58a0d243e0cc462d3cce7308
Ayanda Ngcobo <ayanda13@protonmail.com>
2023-04-27 13:16:09 +0200

    added and styled ChatsHeader, ProfileBtn and UIBtn components

---
+++ src/components/ProfileBtn/ProfileBtn.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import './ProfileBtn.css';
+
+interface ProfileBtnProps {
+    image: string,//this is a link
+    alt: string,//this is the image alt text
+    openProfile: Function //this is a function that will be called when the profileBtn component is clicked. it will open the Profile component.
+}
+
+const ProfileBtn = ({ image, alt, openProfile } : ProfileBtnProps) => {
+
+    //will be called when the profileBtn component is clicked
+    const click = (e: Object) => {
+        if(openProfile) openProfile();
+    }
+
+    return (
+        <button className="profileBtn" onClick={e=>click(e)}>
+            <div>
+                <img className="profileBtn__img" src={image} alt={alt} />
+            </div>
+        </button>
+    )
+}
+
+export default ProfileBtn;
---
+++ src/components/UIBtn/UIBtn.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from "react";
+import './UIBtn.css';
+
+
+interface UIBtnProps {
+    label: string, //the label of the button
+    icon: ReactNode //the svg icon,
+    onClick: Function //the click handler
+}
+
+const UIBtn = ( { label, icon, onClick } : UIBtnProps ) => {
+
+    //will be called when the button is clicked
+    const click = (e: Object) => {
+        if(onClick) onClick();
+    }
+
+    return (
+        <button className="ui-button" onClick={e=>click(e)}>
+            <div>
+                {icon}
+            </div>
+        </button>
+    )
+}
+
+export default UIBtn;
